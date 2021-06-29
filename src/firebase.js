6624b6506c98c69cfd924ba9a6ca313a61208023
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-06-29 20:52:04 +0530

    Create firebase.js file to connect with firebase.

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyC4n_U478NJTIy8aqKwB3ivUfRtfiJMSz4",
+    authDomain: "contact-register-crud.firebaseapp.com",
+    databaseURL: "https://contact-register-crud-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "contact-register-crud",
+    storageBucket: "contact-register-crud.appspot.com",
+    messagingSenderId: "322323713512",
+    appId: "1:322323713512:web:af0f5e5f0f38954cd3a85d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
