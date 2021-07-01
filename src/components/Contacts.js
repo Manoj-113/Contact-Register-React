630998404020a9d3ea0fcc5970c842a7f9a0b320
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-07-01 20:36:09 +0530

    Updated

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import ContactForm from "./ContactForm"
+
 const Contacts = () => {
     return ( 
+        <>
         <div class = "jumboltron jumboltron-fluid">
             <div class = "container">
                 <h1 class = "display-4">Contact Register</h1>
             </div>
         </div>
+
+        <div className = "row">
+            <div className = "col-md-5">
+                <ContactForm/>
+            </div>
+            <div className = "col-md-7">
+                <div>list of contacts</div>
+            </div>
+        </div>
+        </>
+        
      );
 }
  
