745aa48e26eb4fab2cbfbf82cee21a35abd347b5
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-07-01 20:24:27 +0530

    Updated

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,6 +1,10 @@
 const Contacts = () => {
     return ( 
-        
+        <div class = "jumboltron jumboltron-fluid">
+            <div class = "container">
+                <h1 class = "display-4">Contact Register</h1>
+            </div>
+        </div>
      );
 }
  
