f432d07613f0aa1159720a75cf21b0ece9bbd76d
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-07-02 11:27:28 +0530

    Updated

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -22,6 +22,29 @@ const ContactForm = () => {
                 </div>
                 <input className="form-control" placeholder="Full Name" name="fullName" value={values.fullName}></input>
             </div>
+            <div className = "form-row">
+                <div className="form-group input-group col-md-6">
+                    <div className="input-group-prepend">
+                        <div className="input-group-text">
+                            <i className="fas fa-mobile-alt"></i>
+                        </div>
+                    </div>
+                <input className="form-control" placeholder="Mobile" name="mobile" value={values.mobile}></input>
+                </div>
+            </div>
+
+            <div>
+
+            <div className="form-group input-group col-md-6">
+                    <div className="input-group-prepend">
+                        <div className="input-group-text">
+                            <i className="fas fa-envelope"></i>
+                        </div>
+                    </div>
+                <input className="form-control" placeholder="E-mail" name="email" value={values.email}></input>
+                </div>
+
+            </div>
         </form>
      );
 }
