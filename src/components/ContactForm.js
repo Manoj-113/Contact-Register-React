47fb74c6e42c27d4312c0efd78f338df091e851c
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-07-02 11:41:19 +0530

    Updated

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,6 +12,14 @@ const ContactForm = () => {
 
     var [values, setValues] = useState(initialFieldValues)
 
+    const handleInputChange = e => {
+        var {name, value} = e.target
+        setValues({
+            values,
+            [name]: value
+        })
+    }
+
     return ( 
         <form autoComplete="off">
             <div className="form-group input-group">
@@ -20,7 +28,7 @@ const ContactForm = () => {
                         <i className="fas fa-user"></i>
                     </div>
                 </div>
-                <input className="form-control" placeholder="Full Name" name="fullName" value={values.fullName}></input>
+                <input className="form-control" placeholder="Full Name" name="fullName" value={values.fullName} onChange={handleInputChange}></input>
             </div>
             <div className = "form-row">
                 <div className="form-group input-group col-md-6">
@@ -29,22 +37,30 @@ const ContactForm = () => {
                             <i className="fas fa-mobile-alt"></i>
                         </div>
                     </div>
-                <input className="form-control" placeholder="Mobile" name="mobile" value={values.mobile}></input>
+                <input className="form-control" placeholder="Mobile" name="mobile" value={values.mobile} onChange={handleInputChange}></input>
                 </div>
             </div>
 
             <div>
 
+            
+
+            </div>
             <div className="form-group input-group col-md-6">
                     <div className="input-group-prepend">
                         <div className="input-group-text">
                             <i className="fas fa-envelope"></i>
                         </div>
                     </div>
-                <input className="form-control" placeholder="E-mail" name="email" value={values.email}></input>
-                </div>
-
+                <input className="form-control" placeholder="E-mail" name="email" value={values.email} onChange={handleInputChange}></input>
             </div>
+
+                <div className = "form-group">
+                    <textarea className="form-control" placeholder="Address" name="address" value={values.address} onChange={handleInputChange}></textarea>
+                    <div className="form-control">
+                        <input type="submit" value= "Save" className="btn btn-primary btn-block"></input>
+                    </div>
+                </div>
         </form>
      );
 }
