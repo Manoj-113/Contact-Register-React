1766a138ce76e7c44ccd88e7e15c496ecb10418b
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-07-02 10:59:30 +0530

    Updated

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,8 +1,28 @@
-import React from 'react';
+
+import React, {useState, useEffect} from 'react';
 
 const ContactForm = () => {
+
+    const initialFieldValues = {
+        fullName: "",
+        mobile: "",
+        email: "",
+        address: ""
+    }
+
+    var [values, setValues] = useState(initialFieldValues)
+
     return ( 
-<div>Displaying contact</div>
+        <form autoComplete="off">
+            <div className="form-group input-group">
+                <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <i className="fas fa-user"></i>
+                    </div>
+                </div>
+                <input className="form-control" placeholder="Full Name" name="fullName" value={values.fullName}></input>
+            </div>
+        </form>
      );
 }
  
